6e36fa2763b2371746782151aae5dc6f00d50fd7
andrey <andrey@sublimestudio.es>
2025-05-27 17:53:21 +0200

    --wip-- [skip ci]

---
+++ src/views/LoginView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Iniciar sesión</h1>
+        <input type="email">
+        <input type="password">
+        <button>Entrar</button>
+    </div>
+</template>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        register() {
+            console.log(this.email, this.password)
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Registro</h1>
+        <input v-model="email" type="email">
+        <input v-model="password" type="password">
+        <button @click="register">Registrarme</button>
+    </div>
+</template>
